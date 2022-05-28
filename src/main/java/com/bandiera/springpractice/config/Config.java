266c12f12463d46e650ca8cff085daf1a0c3bc83
bandiera <bandiera81@gmail.com>
2022-05-28 16:31:46 +0900

    Config #1 - spring configuration 기초 설정 루트 패키지 이하에 빈이 있는지 스캔한다.

---
+++ src/main/java/com/bandiera/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.bandiera.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.bandiera.springpractice")
+@Configuration
+public class Config {
+}
