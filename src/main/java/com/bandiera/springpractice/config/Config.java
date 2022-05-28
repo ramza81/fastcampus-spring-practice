4229ff6dba7016176685ae368ddfeaf593f678df
bandiera <bandiera81@gmail.com>
2022-05-28 17:40:16 +0900

    Change #1 - configure spring webmcv to gradle 스프링 webmcv를 적용한다.

--- src/main/java/com/bandiera/springpractice/config/Config.java
+++ src/main/java/com/bandiera/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.bandiera.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.bandiera.springpractice")
 @Configuration
 public class Config {
